5503509728d4264bed9feb991ed297803492e6ab
Troy Cheung <TroyC.public@gmail.com>
2016-01-16 13:55:58 +0800

    Solved Prob.004

---
+++ PB004/Problem.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjectEuler
+{
+    public class Algorithm : IProblem
+    {
+        public string compute()
+        {
+            int max = int.MinValue;
+            for (int i = 100; i <= 999; i++)
+                for (int j = i; j <= 999; j++)
+                    if (Troy.isPalindrome((i * j).ToString()))
+                        if (i * j > max)
+                            max = i * j;
+            return max.ToString();
+        }
+
+        public bool prepare()
+        {
+            return true;
+        }
+    }
+}
