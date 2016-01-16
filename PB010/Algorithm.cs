da9c1b8df11365bfaed9fa161479a74187daf458
Troy Cheung <TroyC.public@gmail.com>
2016-01-16 17:23:38 +0800

    Solved Prob.010

---
+++ PB010/Algorithm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    public class Algorithm : IProblem
+    {
+        public string compute()
+        {
+            long sum = 0;
+            foreach (int p in Troy.GetPrimes(2000000 - 1))
+                sum += p;
+            return sum.ToString();
+        }
+
+        public bool prepare()
+        {
+            return true;
+        }
+    }
+}
