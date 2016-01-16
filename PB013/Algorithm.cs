133458e66bee336db3d4bfcdc685cad72a7e40f3
Troy Cheung <TroyC.public@gmail.com>
2016-01-17 01:26:45 +0800

    Solved Prob. 013

---
+++ PB013/Algorithm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Resources;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    public class Algorithm : IProblem
+    {
+        List<BigInteger> data = new List<BigInteger>();
+
+        public string compute()
+        {
+            BigInteger sum = 0;
+            foreach (BigInteger x in data)
+                sum += x;
+            return sum.ToString().Remove(10);
+        }
+
+        public bool prepare()
+        {
+            foreach (string n in Properties.Resources.data.Split('\n'))
+                data.Add(BigInteger.Parse(n));
+            return true;
+        }
+    }
+}
