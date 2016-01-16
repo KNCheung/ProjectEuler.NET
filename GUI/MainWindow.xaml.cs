a938ab713b975e04bcc5d9bc1960f7e44500bfe2
Troy Cheung <TroyC.public@gmail.com>
2016-01-16 17:21:02 +0800

    Added GUI

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ProjectEuler;
+
+namespace GUI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            newProblemSelected();
+        }
+
+        private void problemList_Initialized(object sender, EventArgs e)
+        {
+            problemList.ItemsSource = new PBItemArr();
+            problemList.SelectedValuePath = "id";
+            problemList.DisplayMemberPath = "name";
+            problemList.SelectedValue = Toolbox.LatestProblem;
+        }
+
+        private void problemSelected(object sender, SelectionChangedEventArgs e)
+        {
+            newProblemSelected();
+        }
+
+        private void newProblemSelected()
+        {
+            if (problemList.SelectedValue != null)
+                btnRun.Content = string.Format("Run Problem {0:d3}", (int)problemList.SelectedValue);
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            ((PBItemArr)problemList.ItemsSource).init();
+            problemList.SelectedValue = Toolbox.LatestProblem;
+        }
+    }
+}
---
+++ GUI/PBItem.cs
@@ -0,0 +1,35 @@
+﻿using ProjectEuler;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    public class PBItem
+    {
+        public string name { set; get; }
+        public int id { set; get; }
+    }
+
+    public class PBItemArr: ObservableCollection<PBItem>
+    {
+        public PBItemArr()
+        {
+            this.init();
+        }
+
+        public void init()
+        {
+            this.Clear();
+            Console.WriteLine("Loading problem list");
+            foreach (int x in Toolbox.AvailableProblems)
+            {
+                Console.WriteLine("Found Problem {0:d3}", x);
+                this.Add(new PBItem { name = string.Format("Problem {0:d3}", x), id = x });
+            }
+        }
+    }
+}
