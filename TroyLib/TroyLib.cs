86b451f81cb8c1808fbcf0934d9482c94e166ec7
Troy Cheung <TroyC.public@gmail.com>
2016-01-16 15:24:49 +0800

    Added Primes Iterator. Added GCD-function.

--- TroyLib/TroyLib.cs
+++ TroyLib/TroyLib.cs
@@ -1,9 +1,43 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ProjectEuler
 {
     public static class Troy
     {
+        public static long GCD(long a, long b)
+        {
+            if (b == 0)
+                return a;
+            return GCD(b, a % b);
+        }
+
+        public static bool isPrime(int n)
+        {
+            if (n == 2)
+                return true;
+            if ((n <= 1) || ((n & 1) == 0))
+                return false;
+            for (int i = 3; i <= (int)Math.Ceiling(Math.Sqrt(n)); i += 2)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+
+        public static IEnumerable<int> PrimeIterator(Func<int, bool> isPrime, int start=0)
+        {
+            if (start <= 2)
+            {
+                yield return 2;
+                start = 3;
+            }
+            if ((start & 1) == 0)
+                start++;
+            for (; ; start+=2)
+                if (isPrime(start))
+                    yield return start;
+        }
+
         public static bool isPalindrome(string str)
         {
             for (int i = 0; i < (str.Length >> 1); i++)
