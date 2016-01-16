678748264e1bccabed2621094af4fb2348a55a0d
Troy Cheung <TroyC.public@gmail.com>
2016-01-17 01:55:46 +0800

    Solved Prob.014

---
+++ PB014/Algorithm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+    public class Algorithm : IProblem
+    {
+        Dictionary<long, long> link = new Dictionary<long, long>();
+
+        private long Next(long n)
+        {
+            if ((n & 1) == 0)
+                return n >> 1;
+            else
+                return 3 * n + 1;
+        }
+
+        private long iter(long n)
+        {
+            if (!link.ContainsKey(n))
+                link.Add(n, iter(Next(n)) + 1);
+            return link[n];
+        }
+
+        public string compute()
+        {
+            long maxLength = long.MinValue;
+            long maxN = 0;
+            long length = 0;
+            link.Add(1, 1);
+            for (long i = 1; i < 1000000; i++)
+            {
+                length = iter(i);
+                if (length > maxLength)
+                {
+                    maxLength = length;
+                    maxN = i;
+                }
+            }
+            return maxN.ToString();
+        }
+
+        public bool prepare()
+        {
+            return true;
+        }
+    }
+}
