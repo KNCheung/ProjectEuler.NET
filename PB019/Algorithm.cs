6b84168a343165009fee6fb3091d5d07924ce5fe
Troy Cheung <TroyC.public@gmail.com>
2016-01-18 18:26:32 +0800

    Solved Prob.019

---
+++ PB019/Algorithm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    public class Algorithm : IProblem
+    {
+        public string compute()
+        {
+            int cnt = 0;
+            for (int year = 1901; year <= 2000; year++)
+                for (int month = 1; month <= 12; month++)
+                    if (new DateTime(year, month, 1).DayOfWeek == DayOfWeek.Sunday)
+                        cnt++;
+            return cnt.ToString(); 
+        }
+
+        public bool prepare()
+        {
+            return true;
+        }
+    }
+}
