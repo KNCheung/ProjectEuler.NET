fb0f73cd69405d0bc8416f602eaad5f008e3a91e
Troy Cheung <TroyC.public@gmail.com>
2016-01-18 18:31:05 +0800

    Solved Prob.020

---
+++ PB020/Algorithm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    public class Algorithm : IProblem
+    {
+        public string compute()
+        {
+            BigInteger factorial = BigInteger.One;
+            for (int i = 1; i <= 100; i++)
+                factorial *= i;
+            return Enumerable.Sum<Char>(factorial.ToString().ToCharArray(), (x) => ((int)(x - '0'))).ToString();
+        }
+
+        public bool prepare()
+        {
+            return true;
+        }
+    }
+}
