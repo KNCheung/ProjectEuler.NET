b46ecc1f3863e9d65e40b8b2c6b9f9cafbac2616
Troy Cheung <TroyC.public@gmail.com>
2016-01-18 18:15:56 +0800

    Solved Prob.016

---
+++ PB016/Algorithm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    public class Algorithm : IProblem
+    {
+        public string compute()
+        {
+            BigInteger n = 1;
+            for (int i = 0; i < 1000; i++)
+                n *= 2;
+            return Enumerable.Sum<Char>(n.ToString().ToCharArray(), (x) => ((int)(x - '0'))).ToString();
+        }
+
+        public bool prepare()
+        {
+            return true;
+        }
+    }
+}
