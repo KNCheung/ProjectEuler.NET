565f1ac3cbb292bc3f5358832269cdb7c2c58384
Troy Cheung <TroyC.public@gmail.com>
2016-01-18 16:04:07 +0800

    implement Progressbar

---
+++ GUI/DataSource.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProjectEuler;
+using System.Windows.Shell;
+using System.ComponentModel;
+
+namespace GUI
+{
+    internal class DataSource: INotifyPropertyChanged
+    {
+        public double Progress_MaxValue { set; get; }
+        public double Progress_MinValue { set; get; }
+        public double Progress_CurrentValue { set; get; }
+
+        private CProgress ProgressSource = null;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DataSource()
+        {
+            Progress_MaxValue = 100;
+            Progress_MinValue = 0;
+            Progress_CurrentValue = 0;
+        }
+
+        public void SetProgressSource(CProgress source)
+        {
+            ProgressSource = source;
+            Update();
+        }
+
+        public void Update()
+        {
+            if (ProgressSource != null)
+            {
+                Progress_CurrentValue = ProgressSource.CurrentValue;
+                Progress_MaxValue = ProgressSource.MaxValue;
+                Progress_MinValue = ProgressSource.MinValue;
+
+                OnPropertyChanged(null);
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public void TimerUpdate(Object stateInfo)
+        {
+            Update();
+        }
+    }
+}
--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Shell;
@@ -23,13 +24,25 @@ namespace GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TaskbarItemInfo taskbar = new TaskbarItemInfo();
+        private DataSource data = new DataSource();
+        private Timer updateTimer = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            DataBinding(data);
             newProblemSelected();
-            this.TaskbarItemInfo = new TaskbarItemInfo();
-            this.TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Error;
-            this.TaskbarItemInfo.ProgressValue = 1;
+        }
+
+        private void DataBinding(DataSource data)
+        {
+            this.TaskbarItemInfo = taskbar;
+
+            PBar.SetBinding(ProgressBar.MaximumProperty, new Binding() { Source = data, Path = new PropertyPath("Progress_MaxValue"), Mode = BindingMode.OneWay });
+            PBar.SetBinding(ProgressBar.MinimumProperty, new Binding() { Source = data, Path = new PropertyPath("Progress_MinValue"), Mode = BindingMode.OneWay });
+            PBar.SetBinding(ProgressBar.ValueProperty, new Binding() { Source = data, Path = new PropertyPath("Progress_CurrentValue"), Mode = BindingMode.OneWay });
+
         }
 
         private void problemList_Initialized(object sender, EventArgs e)
@@ -55,6 +68,23 @@ namespace GUI
         {
             ((PBItemArr)problemList.ItemsSource).init();
             problemList.SelectedValue = Toolbox.LatestProblem;
+        }
+
+        Task mainTask = null;
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            if ((mainTask == null) || (mainTask.IsCompleted))
+            {
+                Problem prob = new Problem((int)problemList.SelectedValue);
+                data.SetProgressSource(prob.Progress);
+
+                prob.prepare();
+                mainTask = Task.Run(() => { prob.run(); });
+                updateTimer = new Timer(data.TimerUpdate, null, 0, 50);
+                btnRun.Content = "Running";
+            }
+            data.Update();
+            Console.WriteLine("{0} {1}", PBar.Value, PBar.Maximum);
         }
     }
 }
