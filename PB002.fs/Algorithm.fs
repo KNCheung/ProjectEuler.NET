b70f92422f5fe5487be734a52aec8788b794e32f
Troy Cheung <TroyC.public@gmail.com>
2016-01-19 15:17:13 +0800

    Solved Prob.002 using F#

---
+++ PB002.fs/Algorithm.fs
@@ -0,0 +1,22 @@
+﻿namespace ProjectEuler
+
+type public Algorithm() = class
+    let fibonacci = 
+        let rec iter a b =
+            seq {
+                yield a
+                yield! iter b (a + b)
+            }
+        iter 1 2
+
+    let run = 
+        fibonacci 
+        |> Seq.takeWhile (fun (x) -> (x <= 4000000))
+        |> Seq.filter (fun (x) -> ((x &&& 1) = 0))
+        |> Seq.sum
+ 
+    interface IAlgorithm with
+        member this.Compute(): string = run.ToString()
+        member this.Prepare(): bool = true
+    end
+end
