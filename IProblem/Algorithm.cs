6f69dc340e90c746b7334662264bddafd8a496c1
Beobachter70 <beobachter70@163.com>
2016-01-19 16:58:34 +0800

    Solved Prob.004 using F#

---
+++ IProblem/Algorithm.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Reflection;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ProjectEuler
+{
+    public class AlgorithmFileInfo
+    {
+        private FileInfo file = null;
+
+        public string FileName
+        {
+            get
+            {
+                return file.FullName;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return file.Name;
+            }
+        }
+
+        public int n { private set; get; }
+
+        public AlgorithmLanguage lang { private set; get; }
+
+        public AlgorithmFileInfo(FileInfo file = null)
+        {
+            if (file == null)
+                throw new ArgumentException("Missing Argument: file");
+            this.file = file;
+            this.n = int.Parse(Regex.Match(file.Name, @"[1-9]\d*").Value);
+            switch ((file.Name.Split('.')[1].ToLower()))
+            {
+                case "cs":
+                    this.lang = AlgorithmLanguage.CSharp;
+                    break;
+                case "fs":
+                    this.lang = AlgorithmLanguage.FSharp;
+                    break;
+                default:
+                    this.lang = AlgorithmLanguage.Unknown;
+                    break;
+            }
+        }
+
+        public AlgorithmFileInfo(int n, AlgorithmLanguage lang)
+        {
+            string suffix;
+            this.n = n;
+            this.lang = lang;
+            switch (lang)
+            {
+                case AlgorithmLanguage.CSharp:
+                    suffix = ".cs";
+                    break;
+                case AlgorithmLanguage.FSharp:
+                    suffix = ".fs";
+                    break;
+                default:
+                    suffix = "";
+                    break;
+            }
+            this.file = new FileInfo(string.Format(@"lib\PB{0:d3}{1}.dll", n, suffix));
+        }
+    }
+
+    public class Algorithm
+    {
+        private object algo = null;
+        private Stopwatch stopwatch = new Stopwatch();
+        private AlgorithmFileInfo file = null;
+
+        public bool isPrepared { private set; get; }
+        public TimeSpan time { private set; get; }
+        public bool available { private set; get; }
+        public int n { 
+            get
+            {
+                return file.n;
+            } }
+        public string answer { private set; get; }
+        public CProgress Progress = null;
+        public AlgorithmLanguage lang = AlgorithmLanguage.Unknown;
+
+        public Algorithm(int n = 0, AlgorithmLanguage lang = AlgorithmLanguage.CSharp)
+        {
+            this.time = TimeSpan.MaxValue;
+            this.available = false;
+            this.answer = "";
+            this.isPrepared = false;
+
+            this.file = new AlgorithmFileInfo(n, lang);
+
+            loadProblem();
+        }
+
+        public Algorithm(AlgorithmFileInfo file)
+        {
+            this.file = file;
+            this.time = TimeSpan.MaxValue;
+            this.available = false;
+            this.answer = "";
+            this.isPrepared = false;
+            loadProblem();
+        }
+
+        public Algorithm(FileInfo file)
+        {
+            if (!Regex.IsMatch(file.Name, @"^PB\d{3}(\.\w+)?\.dll$"))
+                throw new ArgumentException("Not an available file");
+
+            this.file = new AlgorithmFileInfo(file);
+            this.time = TimeSpan.MaxValue;
+            this.available = false;
+            this.answer = "";
+            this.isPrepared = false;
+            loadProblem(); 
+        }
+
+        private void loadProblem()
+        {
+            try
+            {
+                Console.Write("Try to load {0}... ", file.FileName);
+                Assembly asm = Assembly.LoadFrom(file.FileName);
+                Console.Write("OK\nTry to create the Algorithm Object... ");
+                this.algo = asm.CreateInstance("ProjectEuler.Algorithm");
+                Console.WriteLine("OK");
+                this.available = true;
+                if (algo is IProgress)
+                {
+                    Console.WriteLine("Found IProgress interface implement");
+                    Progress = new CProgress();
+                    Progress = ((IProgress)algo).Progress;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed with exception\nMESSAGE: {0}", e.Message);
+                this.available = false;
+                this.algo = null;
+            }
+        }
+
+        public bool Prepare()
+        {
+            if (this.available)
+                this.isPrepared = ((IAlgorithm)algo).Prepare();
+            return this.isPrepared;
+        }
+
+        public void Run()
+        {
+            if (!this.isPrepared)
+                Prepare();
+            if (this.available && this.isPrepared)
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+                this.answer = ((IAlgorithm)this.algo).Compute();
+                stopwatch.Stop();
+                this.time = stopwatch.Elapsed;
+            }
+        }
+    }
+}
---
+++ PB004.fs/Algorithm.fs
@@ -0,0 +1,24 @@
+﻿namespace ProjectEuler
+
+type public Algorithm() = class
+    let isPalindrome (str:string) = 
+        let n = str.Length
+        match str with 
+        | "" -> true
+        | _ -> seq {0..(n >>> 1)}
+            |> Seq.exists (fun i -> str.[i] <> str.[n - i - 1])
+            |> not
+
+    let run =
+        seq {for i in 100..999 do
+                for j in i..999 -> i * j}
+        |> Seq.sortDescending
+        |> Seq.map string
+        |> Seq.filter isPalindrome
+        |> Seq.head
+
+    interface IAlgorithm with
+        member this.Compute(): string = run |> string
+        member this.Prepare(): bool = true
+    end
+end
