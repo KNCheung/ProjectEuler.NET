cc7202e95af2a8fb1fd2bbb85078f734ccf6293f
Troy Cheung <TroyC.public@gmail.com>
2016-01-20 17:04:09 +0800

    Solved Prob.007 using F#

---
+++ PB007.fs/Algorithm.fs
@@ -0,0 +1,15 @@
+﻿namespace ProjectEuler
+
+type public Algorithm() = class
+    let isPrime n = TroyMath.isPrime(n)
+
+    let nthPrime n = 
+        Seq.initInfinite id
+        |> Seq.filter isPrime
+        |> Seq.item (n - 1)
+
+    interface IAlgorithm with
+        member this.Compute(): string = nthPrime 10001 |> string
+        member this.Prepare(): bool = true
+    end
+end
