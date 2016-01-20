8b07f6f780a192f339d050ff93b136da8982eb0a
Troy Cheung <TroyC.public@gmail.com>
2016-01-20 16:53:17 +0800

    Solved Prob.006 using F#

---
+++ PB006.fs/Algorithm.fs
@@ -0,0 +1,22 @@
+﻿namespace ProjectEuler
+
+type public Algorithm() = class
+    let square x = x * x
+
+    let SumOfSquare n = 
+        [1..n]
+        |> List.map square
+        |> List.sum
+
+    let SquareOfSum n = 
+        [1..n]
+        |> List.sum
+        |> square
+
+    let ans n = (SquareOfSum n) - (SumOfSquare n)
+
+    interface IAlgorithm with
+        member this.Compute(): string = ans 100 |> string
+        member this.Prepare(): bool = true
+    end
+end
